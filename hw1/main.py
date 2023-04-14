603e5fabb0853cf0badc5b586a372ac6c131aa30
Maxwell Jung <jungmaxwell@gmail.com>
2023-04-13 20:16:19 -0700

    simulate 30 runs

--- hw1/main.py
+++ hw1/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import copy
 
 arm_count = 10
 
@@ -8,6 +9,9 @@ class Environment:
     def __init__(self) -> None:
         self.reward = None
         self.mean = np.random.normal(loc=0, scale=1, size=arm_count)
+        
+    def reset(self):
+        self.reward = None
     
     def generate_reward(self, action):
         def pull_arm():
@@ -23,29 +27,39 @@ class Agent:
         self.N = np.zeros(arm_count)
         self.Q = np.zeros(arm_count)
         self.H = np.zeros(arm_count)
-        self.alpha = None
+        self.hyperparam = None
+        
+    def reset(self):
+        self.action = None
+        self.t = 0
+        self.total_reward = 0
+        self.average_reward = None
+        self.N = np.zeros(arm_count)
+        self.Q = np.zeros(arm_count)
+        self.H = np.zeros(arm_count)
+        self.hyperparam = None
 
-    def set_policy(self, policy):
+    def set_policy(self, policy, hyper_param):
         self.policy = policy
+        self.hyperparam = hyper_param
         
-    def greedy(self, N=5*arm_count):
-        if (self.t > N):
+    def greedy(self):
+        if (self.t > self.hyperparam):
             return np.argmax(self.Q)
         else:
             return np.random.randint(arm_count)
         
-    def epsilon_greedy(self, epsilon=0.2):
+    def epsilon_greedy(self):
         rng = np.random.uniform()
-        if rng > epsilon:
+        if rng > self.hyperparam:
             return np.argmax(self.Q)
         else:
             return np.random.randint(arm_count)
 
-    def ucb(self, c=3):
-        return np.argmax(self.Q + c*np.sqrt(np.log(self.t+1)/(self.N+1)))
+    def ucb(self):
+        return np.argmax(self.Q + self.hyperparam*np.sqrt(np.log(self.t+1)/(self.N+1)))
 
-    def gradient(self, a=0.1):
-        self.alpha = a
+    def gradient(self):
         return np.random.choice(arm_count, p=softmax(self.H))
         
     def select_action(self):
@@ -56,27 +70,44 @@ class Agent:
     def accept_reward(self, reward):
         def update_H(a):
             if a == self.action:
-                return self.H[a] + self.alpha*(reward-self.average_reward)*(1-softmax(self.H)[a])
+                return self.H[a] + self.hyperparam*(reward-self.average_reward)*(1-softmax(self.H)[a])
             else:
-                return self.H[a] - self.alpha*(reward-self.average_reward)*(softmax(self.H)[a])
+                return self.H[a] - self.hyperparam*(reward-self.average_reward)*(softmax(self.H)[a])
         self.total_reward += reward
         self.average_reward = self.total_reward/self.t
         self.Q[self.action] = (self.N[self.action]-1)/(self.N[self.action])*self.Q[self.action] + (1/self.N[self.action])*reward
         self.H = np.vectorize(update_H)(np.arange(arm_count))
-
-def main():
-    env = Environment()
-    bandit = Agent()
-    bandit.set_policy(bandit.gradient)
-    time_horizon = 1000
-    
+        
+def simulate(agent: Agent, env: Environment, time_horizon=1000):
     for t in range(time_horizon):
-        bandit.select_action()
-        env.generate_reward(action=bandit.action)
-        bandit.accept_reward(reward=env.reward)
+        agent.select_action()
+        env.generate_reward(action=agent.action)
+        agent.accept_reward(reward=env.reward)
+        
+def evaluate(agent: Agent, env: Environment, policy, hyperparam):
+    agent.set_policy(policy, hyperparam)
+    simulate(agent, env)
+    return np.max(env.mean)*agent.t - agent.total_reward
+
+def test_epsilon_greedy(agent: Agent, env: Environment, e=0.1):
+    agent.reset()
+    env.reset()
+    return evaluate(agent, env, agent.epsilon_greedy, hyperparam=e)
+
+def test_gradient(agent: Agent, env: Environment, a=0.1):
+    agent.reset()
+    env.reset()
+    return evaluate(agent, env, agent.gradient, hyperparam=a)
     
-    print(vars(bandit))
-    print(vars(env))
+def main():
+    sample_size = 30
+    agent = Agent()
+    env = Environment()
     
+    for x in range(1, 100):
+        hyperparam = e = x/100
+        score = np.mean(np.fromfunction(lambda i: test_epsilon_greedy(copy.deepcopy(agent), copy.deepcopy(env), hyperparam), (sample_size,)))
+        print(f'hyperparam: {hyperparam}, score: {score}')
+        
 if __name__ == '__main__':
     main()
