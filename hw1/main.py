07b76a742f6104a8440f61a617a0977b4695b1fe
Maxwell Jung <jungmaxwell@gmail.com>
2023-04-13 17:15:22 -0700

    implement UCB

--- hw1/main.py
+++ hw1/main.py
@@ -4,70 +4,66 @@ arm_count = 10
 
 class Environment:
     def __init__(self) -> None:
-        self.state = None
-        self.reward = 0
+        self.reward = None
         self.mean = np.random.normal(loc=0, scale=1, size=arm_count)
     
-    def process_action(self, action):
+    def generate_reward(self, action):
         def pull_arm():
             return np.random.normal(loc=self.mean[action], scale=1)
-        self.state = self.state
         self.reward = pull_arm()
 class Agent:
     def __init__(self) -> None:
-        self.policy = greedy
-        self.Q = np.zeros(arm_count)
-        self.action = 0
         self.t = 0
+        self.total_reward = 0
+        self.N = np.zeros(arm_count)
+        self.Q = np.zeros(arm_count)
+        self.policy = self.greedy
+        self.action = None
+
+    def set_policy(self, policy):
+        self.policy = policy
         
-    def set_policy(self, name):
-        if name == 'greedy':
-            self.policy = greedy
-        elif name == 'epsilon_greedy':
-            self.policy = epsilon_greedy
-        elif name == 'ucb':
-            self.policy = ucb
-        elif name == 'gradient':
-            self.policy = gradient
+    def greedy(self, N=5*arm_count):
+        if (self.t > N):
+            return np.argmax(self.Q)
         else:
-            self.policy = greedy
+            return np.random.randint(arm_count)
         
-    def select_action(self, state, reward):
-        self.Q[self.action] += reward
-        self.action = self.policy(t=self.t, Q=self.Q)
+    def epsilon_greedy(self, epsilon=0.2):
+        rng = np.random.uniform()
+        if rng > epsilon:
+            return np.argmax(self.Q)
+        else:
+            return np.random.randint(arm_count)
+
+    def ucb(self, c=3):
+        return np.argmax(self.Q + c*np.sqrt(np.log(self.t+1)/(self.N+1)))
+
+    def gradient(self, a):
+        return 0
+        
+    def select_action(self):
+        self.action = self.policy()
+        self.N[self.action] += 1
         self.t += 1
-
-def greedy(t, Q, N=100):
-    if (t > N):
-        return np.argmax(Q)
-    else:
-        return np.random.randint(arm_count)
-    
-def epsilon_greedy(t, Q, epsilon=0.2, N=None):
-    rng = np.random.uniform()
-    if rng > epsilon:
-        return np.argmax(Q)
-    else:
-        return np.random.randint(arm_count)
-
-def ucb(c):
-    return 0
-
-def gradient(a):
-    return 0
+        
+    def accept_reward(self, reward):
+        self.total_reward += reward
+        self.Q[self.action] = (self.N[self.action]-1)/(self.N[self.action])*self.Q[self.action] + (1/self.N[self.action])*reward
 
 def main():
     env = Environment()
     bandit = Agent()
-    bandit.set_policy('epsilon_greedy')
+    bandit.set_policy(bandit.ucb)
     time_horizon = 1000
     
     for t in range(time_horizon):
-        bandit.select_action(state=env.state, reward=env.reward)
-        env.process_action(action=bandit.action)
+        bandit.select_action()
+        env.generate_reward(action=bandit.action)
+        bandit.accept_reward(reward=env.reward)
     
-    print(bandit.Q)
-    print(env.mean)
+    print(vars(bandit))
+    print(vars(env))
     
 if __name__ == '__main__':
     main()
