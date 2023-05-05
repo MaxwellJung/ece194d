7fbb9dc302d4d7d9587435efcf72a7c23cc93d01
Maxwell Jung <jungmaxwell@gmail.com>
2023-05-05 13:04:52 -0700

    set board to be 6x3

--- hw3/main.py
+++ hw3/main.py
@@ -3,19 +3,7 @@ from tetris import Tetris
 
 def main():
     t = Tetris(starting_piece=0)
-    while True:
-        try:
-            print(t.board)
-            print(t.current_piece)
-            t.place_piece(orientation=int(input('orientation:')), location=int(input('location:')))
-        except Tetris.invalidMoveException:
-            print(f'invalid move')
-            continue
-        except Tetris.gameOverException:
-            print(f'game over')
-            break
-        else:
-            t.current_piece = t.select_next_piece()
-
+    t.start_interactive_play()
+    
 if __name__ == '__main__':
     main()
--- hw3/tetris.py
+++ hw3/tetris.py
@@ -10,12 +10,7 @@ class Tetris:
                        [1]])
     
     all_pieces = [piece1, piece2, piece3]
-    
-    def __init__(self, width=3, height=3, starting_piece=None):
-        self.board_width = width
-        self.board_height = height
-        self.board = np.zeros(shape=(height,width))
-        self.current_piece = self.select_next_piece(i=starting_piece, zero_indexed=True)
+    max_piece_height = max([max(p.shape) for p in all_pieces])
     
     @classmethod
     def select_next_piece(cls, i=None, zero_indexed=True):
@@ -25,47 +20,72 @@ class Tetris:
             return cls.all_pieces[i-1]
         return cls.all_pieces[i]
     
-    class invalidMoveException(Exception):
+    class illegalMoveException(Exception):
         pass
     
     class gameOverException(Exception):
         pass
     
+    def __init__(self, playable_width=3, playable_height=3, starting_piece=None):
+        self.playable_area = (playable_height, playable_width)
+        self.line_height = self.playable_area[0]
+        self.board = np.zeros(shape=(playable_height+self.max_piece_height,playable_width))
+        self.current_piece = self.select_next_piece(i=starting_piece, zero_indexed=True)
+        self.score = 0
+        self.game_over = False
+    
     def place_piece(self, orientation=0, location=0):
-        def drop(piece: np.ndarray):
-            piece_height = piece.shape[0]
-            piece_width = piece.shape[1]
-            pad_left = location
-            pad_right = self.board_width-pad_left-piece_width
-            try:
-                piece_buffer = np.pad(piece, ((0,self.board_height),(pad_left,pad_right)))
-            except ValueError:
-                raise self.invalidMoveException
-            
-            for h in range(self.board_height+1):
-                drop_one_block = piece_buffer + (np.pad(self.board, ((piece_height,0),(0,0))))
-                
-                collision = drop_one_block>1
-                if np.any(collision):
-                    break
-                lowest_drop = drop_one_block
-                piece_buffer = np.roll(piece_buffer, shift=1, axis=0)
-                
-            return lowest_drop
+        '''Place current piece using orientation and location.'''
+        piece = np.rot90(self.current_piece, orientation)
+        piece_height = piece.shape[0]
+        piece_width = piece.shape[1]
+        board_height = self.board.shape[0]
+        board_width = self.board.shape[1]
         
-        def clear_complete_rows(board: np.ndarray):
-            complete_rows = np.all(board, axis=1)
+        def drop_piece():
+            '''Try to drop piece. Raises error if illegal move'''
+            pad_top = self.max_piece_height - piece_height
+            pad_bottom = self.line_height
+            pad_left = location
+            pad_right = board_width-pad_left-piece_width
+            
+            try: piece_mask = np.pad(piece, ((pad_top,pad_bottom),(pad_left,pad_right)))
+            except ValueError: raise self.illegalMoveException # illegal orientation/location
+            
+            for h in range(self.line_height, -1, -1): # loop until piece completely drops
+                board_buffer = piece_mask + self.board
+                collision = board_buffer > 1
+                if np.any(collision): break
+                lowest_board = board_buffer
+                piece_mask = np.roll(piece_mask, shift=1, axis=0)
+                
+            self.board = lowest_board
+        
+        def clear_complete_rows():
+            complete_rows = np.all(self.board, axis=1)
             points = np.sum(complete_rows)
             incomplete_rows = np.invert(complete_rows)
-            new_board = np.pad(board[incomplete_rows], ((board.shape[0]-board[incomplete_rows].shape[0],0),(0,0)))
-            return new_board, points
+            cleaned_board = self.board[incomplete_rows]
+            self.board = np.pad(cleaned_board, ((board_height-cleaned_board.shape[0],0),(0,0)))
+            self.score += points
         
-        rotated_piece = np.rot90(self.current_piece, orientation)
-        b = drop(rotated_piece)
-        b, reward = clear_complete_rows(b)
+        drop_piece()
+        clear_complete_rows()
         
-        above_line = b[:-self.board_height]
-        if np.any(above_line):
-            raise self.gameOverException
+        above_line = self.board[:-self.line_height]
+        if np.any(above_line): raise self.gameOverException
         
-        self.board = b[-self.board_height:]
+    def start_interactive_play(self):
+        while not self.game_over:
+            print(self.board)
+            print(self.current_piece)
+            try:
+                self.place_piece(orientation=int(input('orientation:')), location=int(input('location:')))
+            except self.illegalMoveException:
+                print(f'Invalid orientation/location. Try again.')
+            except self.gameOverException:
+                self.game_over = True
+                print(self.board)
+                print(f'Game Over!')
+            else:
+                self.current_piece = self.select_next_piece()
