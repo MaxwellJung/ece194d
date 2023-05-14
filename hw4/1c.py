8e79d8f6a4c9e7303a7546f67b724cce8d86d9fd
Maxwell Jung <jungmaxwell@gmail.com>
2023-05-13 18:00:18 -0700

    increase episode steps to 1mil

--- hw4/1c.py
+++ hw4/1c.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def generate_episode(policy: np.ndarray, p_h=0.55, max_steps=1000):
+def generate_episode(policy: np.ndarray, p_h=0.55, max_steps=1000000):
     state_history = []
     action_history = []
     reward_history = []
@@ -28,7 +28,7 @@ def generate_episode(policy: np.ndarray, p_h=0.55, max_steps=1000):
     
     return state_history, action_history, reward_history
 
-def monte_carlo(policy: np.ndarray, p_h, num_of_episodes=1000, discount=1):
+def monte_carlo(policy: np.ndarray, p_h, num_of_episodes=100, discount=1):
     table = {s: [] for s in range(101)}
     
     for i in range(num_of_episodes):
@@ -41,8 +41,8 @@ def monte_carlo(policy: np.ndarray, p_h, num_of_episodes=1000, discount=1):
     return np.array([np.mean(table[s]) if len(table[s]) > 0 else 0 for s in table])
     
 def main():
-    p_h = 0.25
-    values = monte_carlo(np.load(f'{p_h} policy.npy'), p_h=p_h, num_of_episodes=10000)
+    p_h = 0.55
+    values = monte_carlo(np.load(f'{p_h} policy.npy'), p_h=p_h)
     np.save(f'{p_h} monte_carlo', values)
     
     expected = np.load(f'{p_h} values.npy')
