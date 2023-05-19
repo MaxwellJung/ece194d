fd80213ec482a12a0d8167bdf81bcd76f5bcaad7
Maxwell Jung <jungmaxwell@gmail.com>
2023-05-18 22:28:52 -0700

    terminal interactive version
    fix spacing

--- 2048/main.py
+++ 2048/main.py
@@ -1,8 +1,35 @@
 import numpy as np
-import puzzle
+import logic
 
 def main():
-    puzzle.GameGrid()
+    play()
+    
+transition = {
+    'left': logic.left,
+    'right': logic.right,
+    'up': logic.up,
+    'down': logic.down
+}
+
+def play():
+    grid = logic.new_game(4)
+    while True:
+        for row in grid:
+            print(row)
+        direction = input(f'Direction (left, right, up, down): ')
+        if direction not in transition.keys():
+            print(f'Invalid direction. Please input a valid direction.')
+            continue
+        grid, done = transition[direction](grid)
+        if done:
+            grid = logic.add_two(grid)
+            if logic.game_state(grid) == 'win':
+                print('Win')
+                break
+            if logic.game_state(grid) == 'lose':
+                print('Lose')
+                break
+        
         
 if __name__ == '__main__':
     main()
