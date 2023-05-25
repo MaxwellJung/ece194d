bug fix: fix incorrect q value calculation to reward + next state value

2048/main.py
@@ -72,8 +72,7 @@ def v_hat(state: int, weight: np.ndarray):
 def q_hat(action: int, state: int, weight: np.ndarray):
     all_next_states = get_all_next_states(state, action)
     rewards = [reward(next_state=next_state, current_state=state, current_action=action) \
-             + reward(next_state=next_state, current_state=state, current_action=action) \
-               for next_state in all_next_states]
+             + v_hat(state=next_state, weight=weight) for next_state in all_next_states]
     
     return np.mean(rewards) # E(r+v(s')) when P(s') is uniform
     